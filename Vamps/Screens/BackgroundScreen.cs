61656e48b06f96f17d42cf8d666b4cd17895100b
Dave Arneson <urvaius@gmail.com>
2013-03-21 19:32:29 -0700

    added vamps background

--- Vamps/Screens/BackgroundScreen.cs
+++ Vamps/Screens/BackgroundScreen.cs
@@ -55,7 +55,7 @@ namespace GameStateManagement
             if (content == null)
                 content = new ContentManager(ScreenManager.Game.Services, "Content");
 
-            backgroundTexture = content.Load<Texture2D>(@"Graphics\background");
+            backgroundTexture = content.Load<Texture2D>(@"Graphics\vampsbackground");
         }
 
 
