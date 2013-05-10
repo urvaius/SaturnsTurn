3ae482e75fa47967d3a5a7edb7810e55fea08409
Dave Arneson <urvaius@gmail.com>
2013-05-10 12:47:55 -0700

    added to powerup class. getting thisngs going alot to do

--- SaturnsTurn/PowerUp.cs
+++ SaturnsTurn/PowerUp.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
 
 
 
@@ -11,51 +12,85 @@ namespace SaturnsTurn
     {
         
         public int Shield { get; set; }
-        public Texture2D powerUpTexture;
+        public Texture2D PowerUpTexture;
         public Vector2 Position;
         public bool Active;
+        public List<String> PowerUpType;
         public int Damage { get; set; }
+        public string ShieldPowerUp;
+        public string DamagePowerUp;
+        public string PowerUpName;
 
 
         Viewport viewport;
         //get the width of the projectile
         public int Width
         {
-            get { return powerUpTexture.Width; }
+            get { return PowerUpTexture.Width; }
         }
 
         //height of projectile
         public int Height
         {
-            get { return powerUpTexture.Height; }
+            get { return PowerUpTexture.Height; }
         }
         //how fast projectile moves
         float PowerUpMoveSpeed;
 
 
-        public void Initialize(Viewport viewport, Texture2D texture, Vector2 position)
+        public void Initialize(Viewport viewport, Texture2D texture, Vector2 position,string powerUpName)
         {
-            powerUpTexture = texture;
+
+
+            PowerUpTexture = texture;
             Position = position;
             this.viewport = viewport;
             Active = true;
             this.Damage = Damage;
+            PowerUpName = powerUpName;
+            PowerUpType = new List<String>();
+            PowerUpType.Add(PowerUpName);
 
-            PowerUpMoveSpeed = 10f;
+            
+            
+            
+
+            
+            
         }
+        private void UpdateDamage()
+        {
+            PowerUpMoveSpeed = 10f;
+            Position.X += PowerUpMoveSpeed;
+            if (Position.X + PowerUpTexture.Width / 2 > viewport.Width)
+                Active = false;
+        
+        }
+
 
         public void Update()
         {
+
+            if (PowerUpType.Equals(DamagePowerUp))
+            {
+                UpdateDamage();
+            }
+
             //projectiles move to the right always
-            Position.X += PowerUpMoveSpeed;
+            //Position.X += PowerUpMoveSpeed;
             //deacivate the bullet if it goes out of screen
-            if (Position.X + powerUpTexture.Width / 2 > viewport.Width)
-                Active = false;
+          //  if (Position.X + PowerUpTexture.Width / 2 > viewport.Width)
+            //    Active = false;
 
         }
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(powerUpTexture, Position, null, Color.White, 0f, new Vector2(Width / 2, Height / 2), 1f, SpriteEffects.None, 0f);
+
+
+
+
+
+            spriteBatch.Draw(PowerUpTexture, Position, null, Color.White, 0f, new Vector2(Width / 2, Height / 2), 1f, SpriteEffects.None, 0f);
 
         }
 
